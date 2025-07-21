ca8302d3db242e4ddeabc6f0d92219faa8ed53b3
saikrishna <gunduboinasaikrishna@gmail.com>
2025-07-21 19:51:41 +0530

    python operators

--- operators/assignment.py
+++ operators/assignment.py
@@ -4,7 +4,7 @@ total += 3  # 10+3 = 13
 total -= 2  # 13-2 = 11
 total *= 4  # 11*4 = 44
 total /= 2  # 44/2 = 22.0
-total %= 3  # 22.0%3 = 1.0 , final value of total is 1.0.So in print statements below, we will assign total to 1 for all operations.
+total %= 3  # 22.0%3 = 1.0 , final value of total is 1.0.So in print statements below, it will assign total to 1 for all operations.
 
 add_assign = total
 subtract_assign = total
