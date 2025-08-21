7fb63b92d650feeb02335b0d7efce0774288351c
SaiKrishna329-Dev <gsaikrishna1496@gmail.com>
2025-08-21 08:54:28 +0530

    python practice

--- Data_types/regex/findall.py
+++ Data_types/regex/findall.py
@@ -1,4 +1,5 @@
- #The name findall usually refers to a function from Python's re (regular expressions) module: re.findall. This function searches a string for all non-overlapping matches of a pattern and returns them as a list.
+ #The name findall usually refers to a function from Python's re (regular expressions) module: re.findall. 
+ #This function searches a string for all non-overlapping matches of a pattern and returns them as a list.
 
 # syntax :
 #  re.findall(pattern, string, flags=0)
@@ -26,5 +27,16 @@ text = "Name: Alice, Age: 25; Name: Bob, Age: 30"
 names = re.findall(r'Name: (\w+)', text)
 print(names)
 
+
+import re 
+text = "Python is awesome, isn't it?"
+#words = re.findall(r'\b\w\b', text) # prints only one character word in string()
+#print(words)
+
+result = re.findall(r'\b\w{2}\b', text) # prints only 2 letter word.
+print(result)
+
+re.findall(r'\b\w{3,}\b', text) # prints 3 or more character word
+re.findall(r'\b\w{4,6}\b', text) # prints 4,5,6 character words in the string.
 
 
